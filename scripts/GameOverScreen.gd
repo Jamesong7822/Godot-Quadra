8d2fec544c7f9a6b3ed2da2a72499483043ecc91
Jamie Tan <jamietan.2017@alumni.sota.edu.sg>
2023-02-25 17:00:35 +0800

    Added save to .txt file function when game over

--- scripts/GameOverScreen.gd
+++ scripts/GameOverScreen.gd
@@ -1,6 +1,15 @@
 extends Control
 
+func _ready():
+	saveUserData(Globals.PLAYER_INFO)
 
+func saveUserData(dict):
+	var datetime_string = Time.get_datetime_string_from_system()
+	var filename = "%s.txt" % datetime_string
+	var saveHandle = File.new()
+	saveHandle.open(filename, File.WRITE)
+	saveHandle.store_line(to_json(dict))
+	saveHandle.close()
 
 func _on_BackToMenuButton_pressed() -> void:
 	Globals.changeScene(Globals.mainMenuScene)
