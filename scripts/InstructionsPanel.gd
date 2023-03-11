fe882eeefa8337f67bd0d9b7fc24be91203b8bfb
Jamie Tan <jamietan.2017@alumni.sota.edu.sg>
2023-03-11 15:55:30 +0800

    Fixed server/client roles during collab, updated instruction screens

--- scripts/InstructionsPanel.gd
+++ scripts/InstructionsPanel.gd
@@ -1,8 +1,16 @@
 extends TextureRect
+export (Array, Texture) var InstructionImages
 
 signal finished
 
 func _ready() -> void:
+	if Globals.currentGameType == Globals.GAME_TYPE.INDIVIDUAL:
+		texture = InstructionImages[0]
+	elif Globals.currentGameType == Globals.GAME_TYPE.COLLABORATIVE:
+		if get_tree().is_network_server():
+			texture = InstructionImages[1]
+		else:
+			texture = InstructionImages[2]
 	show()
 	$Timer.wait_time = Globals.instructionsTime
 	$Timer.start()
