4cae7cb4120c7d62f605fb26956dbd1fb0c6cdf7
Wei Song <weisong_ong@mymail.sutd.edu.sg>
2023-04-16 14:12:27 +0800

    Fix typo hehe

--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -240,7 +240,7 @@ func forceCalcInactiveArray() -> void:
 	# (only in collab mode)
 	var inactiveArr = []
 	for block in get_tree().get_nodes_in_group("BLOCKS"):
-		Globals.inactive.append(block.get_parent().position+block.position)
+		inactiveArr.append(block.get_parent().position+block.position)
 	print(inactiveArr == inactive)
 	if inactiveArr != inactive:
 		inactive = inactiveArr
